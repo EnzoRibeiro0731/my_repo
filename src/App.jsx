3c4042216e595a294d5a0cace44754cd5312d3f5
Enzo <ribeiroenzo089@gmail.com>
2024-05-31 19:38:19 -0300

    adicionando informações

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import './global.css'
 import { Projects } from '../src/components/projects/Projects'
 import { Main } from '../src/components/main/Main'
 import { Header } from '../src/components/header/Header'
+import { Infos } from '../src/components/Infos/Infos'
 function App() {
 
   return (
       <div>
       < Header />
-      <Main />
-      <Projects />
+
+        <Main />
+
+        <Projects />
+      
+          <Infos />
       </div>
   )
 }
